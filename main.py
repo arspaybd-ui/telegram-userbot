af4b536024d9db40ae362df6e7e91571bbc7c7ba
arspaybd-ui <arspaybd@gmail.com>
2026-05-23 23:25:55 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import os
 from telethon import TelegramClient, events
+from telethon.sessions import StringSession
 
-# Railway Variables
 api_id = int(os.environ["API_ID"])
 api_hash = os.environ["API_HASH"]
 session = os.environ["SESSION"]
 
-# Client
-client = TelegramClient("session", api_id, api_hash)
+client = TelegramClient(
+    StringSession(session),
+    api_id,
+    api_hash
+)
+
+
+@client.on(events.NewMessage(pattern='/pay'))
+async def pay(event):
+    await event.reply("""
+💳 PAYMENT METHODS
+
+🟣 Bkash
+`01331202837`
+
+🟠 Nagad
+`01957858795`
+""")
+
+
+print("Userbot Running...")
+
+client.connect()
+client.run_until_disconnected()
 
 
 # START COMMAND
