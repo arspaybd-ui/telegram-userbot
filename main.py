d3bbe1f4d0230ed0d7987738105fa4ab9755eb78
arspaybd-ui <arspaybd@gmail.com>
2026-07-06 00:14:27 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,52 @@
-import os
-from telethon import TelegramClient, events
-from telethon.sessions import StringSession
-
-api_id = int(os.environ["API_ID"])
-api_hash = os.environ["API_HASH"]
-session = os.environ["SESSION"]
-
-client = TelegramClient(
-    StringSession(session),
-    api_id,
-    api_hash
-)
-
 @client.on(events.NewMessage(pattern=r'(?i)Apay'))
 async def pay(event):
     await event.reply("""
-╭━━━〔 💳 Pᴀʏᴍᴇɴᴛ Sʏsᴛᴇᴍ 〕━━━╮
+╔══════════════════════╗
+      💳 Pᴀʏᴍᴇɴᴛ Mᴇᴛʜᴏᴅs
+╚══════════════════════╝
 
-🟣 Bᴋᴀsʜ (Mᴇʀᴄʜᴀɴᴛ)
-┏━━━━━━━━━━━━┓
-➥ `01331202837`
-┗━━━━━━━━━━━━┛
+🟣 ʙKᴀsʜ • Mᴇʀᴄʜᴀɴᴛ
+╭────────────────────╮
+│ `01331202837`
+╰────────────────────╯
 
-🟠 Nᴀɢᴀᴅ (Pᴇʀsᴏɴᴀʟ)
-┏━━━━━━━━━━━━┓
-➥ `01957858795`
-┗━━━━━━━━━━━━┛
+🟣 ʙKᴀsʜ • Pᴇʀsᴏɴᴀʟ
+╭────────────────────╮
+│ `01957858795`
+╰────────────────────╯
 
-🔵 Rᴏᴄᴋᴇᴛ (Pᴇʀsᴏɴᴀʟ)
-┏━━━━━━━━━━━━┓
-➥ `01957858795`
-┗━━━━━━━━━━━━┛
+🟠 Nᴀɢᴀᴅ • Pᴇʀsᴏɴᴀʟ
+╭────────────────────╮
+│ `01957858795`
+╰────────────────────╯
 
-🟢 Uᴘᴀʏ (Pᴇʀsᴏɴᴀʟ)
-┏━━━━━━━━━━━━┓
-➥ `01957858795`
-┗━━━━━━━━━━━━┛
+🔵 Rᴏᴄᴋᴇᴛ • Pᴇʀsᴏɴᴀʟ
+╭────────────────────╮
+│ `01957858795`
+╰────────────────────╯
 
-╰━━━━━━━━━━━━━━━━━━━━╯
+🟢 Uᴘᴀʏ • Pᴇʀsᴏɴᴀʟ
+╭────────────────────╮
+│ `01957858795`
+╰────────────────────╯
+
+━━━━━━━━━━━━━━━━━━━━━━
 
 📌 Pᴀʏᴍᴇɴᴛ Sᴇɴᴅ Kᴏʀᴀʀ Pᴏʀ
-Sᴄʀᴇᴇɴsʜᴏᴛ & Tʀx ID Pᴀᴛʜᴀɴ
 
+✓ Sᴄʀᴇᴇɴsʜᴏᴛ
+✓ Tʀᴀɴsᴀᴄᴛɪᴏɴ ID
+✓ Sᴇɴᴅᴇʀ Nᴜᴍʙᴇʀ
+✓ Pᴀɪᴅ Aᴍᴏᴜɴᴛ
+
+━━━━━━━━━━━━━━━━━━━━━━
+
+⚡ Vᴇʀɪғɪᴄᴀᴛɪᴏɴ Tɪᴍᴇ
+1 - 10 Mɪɴᴜᴛᴇs
+
+❤️ Tʜᴀɴᴋ Yᴏᴜ Fᴏʀ Cʜᴏᴏsɪɴɢ
+Aʀs Tᴏᴘᴜᴘ Bᴅ
 """)
-
 @client.on(events.NewMessage)
 async def auto_calc(event):
 
@@ -63,16 +66,18 @@ async def auto_calc(event):
 
         result = eval(text)
 
-        await event.reply(f"""
+       await event.reply(f"""
+╭━━━〔 🧮 Cᴀʟᴄᴜʟᴀᴛᴏʀ 〕━━━╮
+
 ✓ Cᴀʟᴄᴜʟᴀᴛɪᴏɴ Cᴏᴍᴘʟᴇᴛᴇᴅ
 
-➦ Iɴᴘᴜᴛ :
-➥ `{text}`
+➥ Iɴᴘᴜᴛ
+`{text}`
 
-➦ Rᴇsᴜʟᴛ :
-➥ `{result}`
+➥ Rᴇsᴜʟᴛ
+`{result}`
 
-━━━━━━━━━━━━━━━━━━
+╰━━━━━━━━━━━━━━━━━━╯
 """)
 
     except Exception:
